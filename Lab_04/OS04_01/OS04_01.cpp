2db334b388f05a215aba712807cac180537363c5
Nikita <nikitailyin186@gmail.com>
2023-09-28 08:58:39 +0300

    Finished 4th LW

---
+++ Lab_04/OS04_01/OS04_01.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+
+int main()
+{
+    DWORD pid = GetCurrentProcessId();
+    DWORD tid = GetCurrentThreadId();
+
+	for (short i = 1; i <= 10000; ++i)
+	{
+		cout << i << ". PID = " << pid << "       TID = " << tid << "\n";
+		Sleep(1000);
+	}
+	system("pause");
+	return 0;
+}
+
+// 1. Powershell ISE:		(Get-Process OS04_01).Threads
---
+++ Lab_04/OS04_08.c
@@ -0,0 +1,48 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+/*  Info about threads:   ps -Lp <PID> H
+                          ps -fL -p <PID> H
+*/
+
+
+
+void* ChildThread_T1(void* arg)
+{
+    pid_t pid = getpid();
+    pid_t tid = syscall(SYS_gettid);
+    for (int i = 1; i <= 75; ++i)
+    {
+        printf("%d. PID = %d      [CHILD]  TID = %d\n", i, pid, tid);
+        sleep(1);
+        if (i == 50)
+            sleep(10);
+    }
+    pthread_exit("Child thread");
+}
+
+
+
+int main()
+{
+    pid_t pid = getpid();
+    pid_t tid = syscall(SYS_gettid);
+    pthread_t a_thread;
+    void* thread_result;
+    int res = pthread_create(&a_thread, NULL, ChildThread_T1, NULL);
+
+    for (int i = 1; i <= 100; ++i)
+    {
+        printf("%d. PID = %d      [MAIN]   TID = %d\n", i, pid, tid);
+        sleep(1);
+        if (i == 30)
+            sleep(15);
+    }
+
+    int status = pthread_join(a_thread, (void**)&thread_result);
+    exit(0);
+}

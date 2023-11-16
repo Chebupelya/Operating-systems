698a2294bdaad261a0a5f8683fe2d81815d13e1f
Nikita <nikitailyin186@gmail.com>
2023-11-16 08:31:49 +0300

    Finished 8th LW

---
+++ Lab_08/OS06_04/OS06_04.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+
+
+void sh(HANDLE pheap) 
+{
+	PROCESS_HEAP_ENTRY phe;
+	phe.lpData = NULL;
+	int count = 1;
+	while (HeapWalk(pheap, &phe)) 
+	{
+		// R - начало непрерывной области
+		// U - нераспределенная область 
+		// B - распределенная область
+		cout << count << " -- address = " << showbase << hex << phe.lpData <<
+			", size = " << dec << phe.cbData <<
+			((phe.wFlags & PROCESS_HEAP_REGION) ? " R" : "") <<
+			((phe.wFlags & PROCESS_HEAP_UNCOMMITTED_RANGE) ? " U" : "") <<
+			((phe.wFlags & PROCESS_HEAP_ENTRY_BUSY) ? " B" : "") << "\n";
+		count++;
+	}
+}
+
+
+
+int main() 
+{
+	HANDLE pheap = GetProcessHeap();
+	cout << "\n==================  BEFORE  ==================\n\n";
+	sh(pheap);
+
+	int size = 300000;
+	int* m = new int[size];
+	cout << "-- m = " << showbase << hex << m << ", size = " << dec << size << '\n';
+
+	cout << "\n\n==================  AFTER  ==================\n\n";
+	sh(pheap);
+}
